962cec5d41d5ee5e845e892029ac3d4119dbe902
Ben Martinez-Bateman <ben@alwaysblank.org>
2020-11-30 17:50:07 -0800

    Add default args for generators

--- cmds/make.js
+++ cmds/make.js
@@ -1,38 +1,67 @@
 #!/usr/bin/env node
-
+const {
+    DateTime
+} = require("luxon");
 
 exports.command = 'make <command>'
 exports.description = 'Make content, or at least the beginnings of content'
 exports.builder = (yargs) => {
-  const {
-    findConfig
-  } = require('../helpers')
-  const {
-    createMakeCommand
-  } = require('./make/funcs')
-  const {
-    functions: {
-      make: {
-        commands
-      }
+    const {
+        findConfig
+    } = require('../helpers')
+    const {
+        createMakeCommand
+    } = require('./make/funcs')
+    const {
+        functions: {
+            make: {
+                commands
+            }
+        }
+    } = findConfig();
+
+    let newYargs = yargs
+        .commandDir('make')
+        .options({
+            'slug': {
+                alias: 's',
+                requiresArg: true,
+                type: 'string',
+                default: '',
+                description: "Part of the filename and also the URL",
+                defaultDescription: "A sanitized version of the title"
+            },
+            'date': {
+                alias: 'd',
+                type: 'string',
+                requiresArg: true,
+                default: DateTime.local(),
+                defaultDescription: "Current date and time",
+                description: "The date (and optionally time) in an ISO 8061 compatible format",
+                coerce: arg => DateTime.fromISO(arg)
+            },
+            'content': {
+                alias: 'c',
+                type: 'string',
+                requiresArg: true,
+                description: "The content of the entry, in markdown.",
+                default: '',
+                defaultDescription: "An empty string"
+            }
+        });
+
+    if (undefined !== commands && commands.length > 0) {
+        commands.forEach(({
+                              type
+                          }) => {
+            newYargs = newYargs.command(createMakeCommand({
+                name: type
+            }))
+        })
     }
-  } = findConfig();
 
-  let newYargs = yargs
-    .commandDir('make');
-
-  if (undefined !== commands && commands.length > 0) {
-    commands.forEach(({
-      type
-    }) => {
-      newYargs = newYargs.command(createMakeCommand({
-        name: type
-      }))
-    })
-  }
-
-  return newYargs;
+    return newYargs;
 }
 exports.handler = function (argv) {
-  console.log(argv)
+    console.log(argv)
 }

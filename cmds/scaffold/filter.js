c49298a2a6b73e1527bac72159ab3ad5a554b214
Ben Martinez-Bateman <ben@alwaysblank.dev>
2021-02-10 19:53:42 -0800

    Fixes scaffolding issue with loader creation

--- cmds/scaffold/filter.js
+++ cmds/scaffold/filter.js
@@ -1,49 +1,54 @@
 const {
-  logError,
-  logSuccess,
-  filePath,
-  makeRelative,
-  makeSafe,
-  getSubcommand
+    logError,
+    logSuccess,
+    filePath,
+    makeRelative,
+    makeSafe,
+    getSubcommand
 } = require('../../helpers')
 const {
-  createStub,
-  insertIntoLoader
+    createStub,
+    insertIntoLoader
 } = require('./funcs')
 
 exports.command = 'filter <name>'
 exports.desc = 'Scaffold a new filter or filter collection'
 exports.builder = (yargs) => {
-  return yargs
-    .example([
-      [`$0 ${getSubcommand(yargs)} spaceship_name`, "Creates a filter called 'spaceship_name'"],
-      [`$0 ${getSubcommand(yargs)} spaceship_name --stub /home/user/file.stub`, "Creates an filter from a custom stub instead of the default"],
-    ])
+    return yargs
+        .example([
+            [`$0 ${getSubcommand(yargs)} spaceship_name`, "Creates a filter called 'spaceship_name'"],
+            [`$0 ${getSubcommand(yargs)} spaceship_name --stub /home/user/file.stub`, "Creates an filter from a custom stub instead of the default"],
+        ])
 }
 exports.handler = function (argv) {
-  const {
-    join
-  } = require('path');
-  const safeName = makeSafe(argv.name)
-  const fileName = `${safeName}.js`
-  const stubPath = join(__dirname, 'stubs', 'filter.stub');
-  const filterDir = join(process.cwd(), '11ty', 'filters');
-  const filterFile = join(filterDir, fileName);
-  const filterIndex = join(filterDir, 'loader.js');
+    const {
+        join
+    } = require('path');
+    const safeName = makeSafe(argv.name)
+    const fileName = `${safeName}.js`
+    const stubPath = join(__dirname, 'stubs', 'filter.stub');
+    const filterDir = join(process.cwd(), '11ty', 'filters');
+    const filterFile = join(filterDir, fileName);
+    const filterIndex = join(filterDir, 'loader.js');
 
-  // Create the filter file
-  createStub({
-      stub: stubPath,
-      destination: filterFile,
-      argv
+    // Create the filter file
+    createStub({
+        stub: stubPath,
+        destination: filterFile,
+        argv
     })
-    .then(() => insertIntoLoader(filterIndex, `./${fileName}`, `module.exports = conf => {`))
-    .then(() => logSuccess(`${argv.name} added in ${filePath(makeRelative(filterFile))}`))
-    .then(() => {
-      if (argv.open) {
-        const edit = require('open-editor')
-        edit([filterFile])
-      }
-    })
-    .catch(error => logError(error))
+        .then(() => insertIntoLoader({
+            loaderPath: filterIndex,
+            confPath: `./${fileName}`,
+            insertAfter: `module.exports = conf => {`,
+            requireStatement: `require('./${makeRelative(filterFile)}'),`
+        }))
+        .then(() => logSuccess(`${argv.name} added in ${filePath(makeRelative(filterFile))}`))
+        .then(() => {
+            if (argv.open) {
+                const edit = require('open-editor')
+                edit([filterFile])
+            }
+        })
+        .catch(error => logError(error))
 }
